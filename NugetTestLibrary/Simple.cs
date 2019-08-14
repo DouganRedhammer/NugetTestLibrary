31c48890c9d4ca86c7897308c3c9ac8b88debb00
Daniel Franklin <daniel.johann.franklin@gmail.com>
2019-08-14 15:02:10 -0700

    Nuget Test Library
    Nuget Test Library

---
+++ NugetTestLibrary/Simple.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetTestLibrary
+{
+    public static class Simple
+    {
+        public static string HelloWorld()
+        {
+            return "HelloWorld";
+        }
+
+        public static string Echo(string echo)
+        {
+            return echo;
+        }
+    }
+}
